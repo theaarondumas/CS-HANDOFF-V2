a337dbc4bd921c56af7451054fd0e7d2d3d24e10
theaarondumas <aardumas@gmail.com>
2026-05-08 23:42:37 +0000

    Fix SVG apple touch icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,9 +12,9 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       {
-        src: '/icons/apple-touch-icon.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: '/apple-touch-icon.png',
+        sizes: 'any',
+        type: 'image/svg+xml',
         purpose: 'maskable',
       },
     ],
