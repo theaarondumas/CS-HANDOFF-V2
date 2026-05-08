08a7392048af40b3bd5dfeeb048a2f0c7d4618e1
theaarondumas <aardumas@gmail.com>
2026-05-08 23:13:52 +0000

    Fix PWA manifest icon purpose type

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icons/cs-handoff-icon.svg',
         sizes: 'any',
         type: 'image/svg+xml',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
   }
